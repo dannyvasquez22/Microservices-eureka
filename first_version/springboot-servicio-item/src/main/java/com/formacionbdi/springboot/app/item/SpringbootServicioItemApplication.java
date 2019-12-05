4e0efa3cf0b2ce80f2bf66de61c80b6790f74607
Danny Vasquez <dani22_vr@hotmail.com>
2019-12-05 00:08:54 -0500

    Version avanzada de microservices

--- first_version/springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ first_version/springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -1,10 +1,22 @@
 package com.formacionbdi.springboot.app.item;
 
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import pe.bwit.tpd.migracion.consultadatoscliente.ws.types.auditschema.AuditRequest;
+import pe.bwit.tpd.migracion.consultadatoscliente.ws.types.schema.ConsultarRequest;
+import pe.bwit.tpd.migracion.consultadatoscliente.ws.types.schema.ConsultarResponse;
+import pe.bwit.tpd.migracion.consultadatoscliente.ejb.sb.DatosClienteEJBRemote;
 
 @EnableCircuitBreaker
 @EnableEurekaClient
@@ -12,8 +24,50 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
 
+	private static Context ctx;
+	// PRIMERA FORMA
+	public static void setUp() throws NamingException {
+		Properties env = new Properties();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.RemoteInitialContextFactory");
+		env.put(Context.PROVIDER_URL, "ejbd://localhost:4201");
+		ctx = new InitialContext(env);
+	}
+	
+	public void executeRemoteEJBTest() throws NamingException {
+		DatosClienteEJBRemote ejbRemote = (DatosClienteEJBRemote) ctx.lookup("DatosClienteEJBRemote");
+		ConsultarRequest request = new ConsultarRequest();
+		request.setAuditRequest(new AuditRequest());
+		request.setNroDoc("12312312");
+		request.setTipoDoc("DNI");
+		ConsultarResponse response = ejbRemote.consultarTipoLineaDatosCliente(new ConsultarRequest());
+		System.out.println(response);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioItemApplication.class, args);
 	}
+
+	// SEGUNDA FORMA
+	@Bean
+	public Context context() throws NamingException {
+		Properties jndiProps = new Properties();
+		jndiProps.put("java.naming.factory.initial", "weblogic.jndi.WLInitialContextFactory");
+//        jndiProps.put("jboss.naming.client.ejb.context", true);
+		jndiProps.put("java.naming.provider.url", "t3://127.0.0.1:7003");
+		return new InitialContext(jndiProps);
+	}
+
+	private String getFullName(Class classType) {
+		String moduleName = "spring-ejb-remote/";
+		String beanName = classType.getSimpleName();
+		String viewClassName = classType.getName();
+
+		return moduleName + beanName + "!" + viewClassName;
+	}
+
+//	@Bean
+//	public ConsultarResponse consultarTipoLineaDatosCliente(Context context) throws NamingException {
+//		return (ConsultarResponse) context.lookup();
+//	}
 
 }
